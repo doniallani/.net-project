084fa5e53b88dcd457cd66bb9a9f08c3b8150744
doniallani <50177827+doniallani@users.noreply.github.com>
2019-08-01 21:42:30 +0100

    API with asp.net

---
+++ API/Controllers/InventoryController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("v1/")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+
+        private readonly InventoryService _service;
+        [HttpPost]
+        [Route ("AddInventoryItems")]
+        public ActionResult<InventoryItems> AddInventoryItems(InventoryItems items)
+        {
+            var inventoryItems = _service.AddInventoryItems(items);
+
+            if (inventoryItems == null)
+            {
+                return NotFound();
+            }
+            return inventoryItems;
+        }
+            [HttpGet]
+            [Route ("GetInventoryItems")]
+            public ActionResult <Dictionary<string, InventoryItems>> GetInventoryItems()
+        {
+            var inventoryItems = _service.GetInventoryItems();
+            if (inventoryItems.Count == 0)
+                return NotFound();
+            return inventoryItems;
+        }
+        }
+    }
---
+++ API/Services/InventoryService.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class InventoryService : InventoryServices
+
+    {
+        private readonly Dictionary<String, InventoryItems> _inventoryItems;
+        public InventoryService()
+        {
+            _inventoryItems = new Dictionary<String, InventoryItems>();
+        }
+        public InventoryItems AddInventoryItems(InventoryItems items)
+        {
+            _inventoryItems.Add(items.ItemName, items);
+            return items;
+        }
+        public Dictionary<String, InventoryItems> GetInventoryItems()
+        {
+            return _inventoryItems;
+            throw new NotImplementedException();
+        }
+    }
+}
